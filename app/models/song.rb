b5d82794035cfb37484579aed5e2ed3fd7742986
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-09 22:06:41 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
+  #solves nil issue when trying to edit songs artist
   def artist_name
-    self.artist.name
+    if artist
+      artist.name
+    end
   end
 
   def artist_name=(name)
