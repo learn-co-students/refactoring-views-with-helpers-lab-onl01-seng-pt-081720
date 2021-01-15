d655704e94a161a718af0e3161ffb957d568c958
Jeremy Wood <js.wood206@gmail.com>
2021-01-15 14:47:16 -0800

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,11 +1,2 @@
 module ArtistsHelper
-
-    def display_artist(artist)
-        binding.pry
-        if !!artist
-            link_to artist.name, artist_path(artist)
-        else
-            link_to "Add Artist", edit_song_path(params[:id])
-        end
-    end
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,6 +1,5 @@
 module SongsHelper
 
-
     def display_artist(song)
         if !!song.artist
             link_to song.artist.name, artist_path(song.artist)
