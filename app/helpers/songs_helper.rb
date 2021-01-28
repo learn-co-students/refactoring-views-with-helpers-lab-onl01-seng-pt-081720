fc7e9d093be9a5a83cb477e13c5ae043061527c4
VQuine <01010110.v@gmail.com>
2021-01-28 11:35:24 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,3 @@
 module SongsHelper
-    def display_artist()
-        if self.artist
-            link_to self.artist_name, artist_path(artist)
-        else
-            link_to "Add Artist", edit_song_path(self)
-        end
-    end
+   
 end
