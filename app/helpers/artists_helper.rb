a620c00582bebffeabbcda0209a636ec1bc11234
Angelica203 <brantleyhomebuyer@gmail.com>
2021-01-29 15:46:34 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,7 @@ module ArtistsHelper
         if song.artist && song.artist.name != ""
             link_to song.artist.name, artist_path(song.artist)
         else
-            link_to "Add Artist", edit_songs_path(song)
+            link_to "Add Artist", edit_song_path(song)
         end
     end
 end
